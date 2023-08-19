475a1d9505cf53d312713e3c99f265561031aa2a
Lindelwa <lindelwa.codes@gmail.com>
2023-08-19 17:56:09 +0200

    Add game() function
    Add game() function that controls the whole game, the function
    is responsible for calling other functions and keeping count
    of the winner of each round and calculating the overall winner

--- script.js
+++ script.js
@@ -66,3 +66,35 @@ const getUserHandShape = () => {
 
   return userChosenHandShape;
 }
+
+const game = () => {
+  let computerScore = 0;
+  let playerScore = 0;
+  let computerSelection;
+  let playerSelection;
+  let winner;
+
+  for (let i = 0; i < 5; i++) {
+    computerSelection = getComputerChoice();
+    playerSelection = getUserHandShape();
+
+    playRound(playerSelection, computerSelection);
+
+    winner = getWinner(playerSelection, computerSelection);
+
+    if (winner === "computer") computerScore++;
+    else if (winner === "player") playerScore++;
+  }
+
+  if (computerScore !== playerScore) {
+    // assign "computer" to variable winner if computerScore is higher than playerScore otherwiser assign "player"
+    winner = computerScore > playerScore ? "computer" : "player";
+    console.log(`
+      The Final Score => player ${playerScore} : ${computerScore} computer
+
+      And the winner is ${winner}!
+    `);
+  }
+
+  else console.log("It's a draw!");
+}
