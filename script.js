e73b87c261fd073272d2832a021323d6ea94ef34
Lindelwa <lindelwa.codes@gmail.com>
2023-08-19 15:51:46 +0200

    Create getWinner()
    Create getWinner() to get the winner between the computer
    and the player

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ const getComputerChoice = () => {
   return choices[randomNumber];
 }
 
+// create a function called getWinner that takes in playerSelection and computerSelection and returns a string that declares the winner
+const getWinner = (playerSelection, computerSelection) => {
+  if 
+    (
+      (computerSelection === "rock" && playerSelection === "scissors")
+      || (computerSelection === "paper" && playerSelection === "rock")
+      || (computerSelection === "scissors" && playerSelection === "paper")
+
+    ) return "computer";
+  
+
+  else if 
+    (
+      (computerSelection === "rock" && playerSelection === "paper")
+      || (computerSelection === "paper" && playerSelection == "scissors")
+      || (computerSelection === "scissors" && playerSelection === "rock")
+    ) return "player";
+}
+
