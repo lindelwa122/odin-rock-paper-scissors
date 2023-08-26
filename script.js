9029c670a07fef0e943bd2d346777c1dc87f77ed
Lindelwa <lindelwa.codes@gmail.com>
2023-08-26 22:26:51 +0200

    Update game()
    Update game() so that now it only gets the winner of the round
    increment the score and round. If round it's round 5, it must
    display the winner [which is still to be done]

--- script.js
+++ script.js
@@ -72,36 +72,21 @@ const getUserHandShape = () => {
   return userChosenHandShape;
 }
 
-const game = () => {
-  let computerScore = 0;
-  let playerScore = 0;
-  let computerSelection;
-  let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let round = 0;
+
+const game = (playerSelection, computerSelection) => {
   let winner;
 
-  for (let i = 0; i < 5; i++) {
-    computerSelection = getComputerChoice();
-    playerSelection = getUserHandShape();
-
-    playRound(playerSelection, computerSelection);
-
-    winner = getWinner(playerSelection, computerSelection);
-
-    if (winner === "computer") computerScore++;
-    else if (winner === "player") playerScore++;
+  if (++round === 6) {
+    // TODO
   }
 
-  if (computerScore !== playerScore) {
-    // assign "computer" to variable winner if computerScore is higher than playerScore otherwiser assign "player"
-    winner = computerScore > playerScore ? "computer" : "player";
-    console.log(`
-      The Final Score => player ${playerScore} : ${computerScore} computer
+  winner = getWinner(playerSelection, computerSelection);
 
-      And the winner is ${winner}!
-    `);
-  }
-
-  else console.log("It's a draw!");
+  if (winner === "computer") computerScore++;
+  else if (winner === "player") playerScore++;
 }
 
 // game();
@@ -113,5 +98,6 @@ playButtons.forEach((playButton) => {
     const playerSelection = playButton.dataset.hand;
     const computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+    game(playerSelection, computerSelection);
   });
 })
