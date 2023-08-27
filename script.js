d8caeda86d5037b8239b7da745b70af562d65a98
Lindelwa <lindelwa.codes@gmail.com>
2023-08-27 20:52:57 +0200

    Format document

--- script.js
+++ script.js
@@ -1,76 +1,41 @@
 const getComputerChoice = () => {
-  // create a list called choices that contains "rock", "paper", and "scissors"
   const choices = ["rock", "paper", "scissors"];
-
-  // create a variable called randomNumber of type number that starts with a random number between 0 and 2
   let randomNumber = Math.floor(Math.random() * 3);
-
-  // return an item at index randomNumber of choices
   return choices[randomNumber];
-}
+};
 
-// create a function called getRoundWinner that takes in playerSelection and computerSelection and returns a string that declares the winner
 const getRoundWinner = (playerSelection, computerSelection) => {
-  if 
-    (
-      (computerSelection === "rock" && playerSelection === "scissors")
-      || (computerSelection === "paper" && playerSelection === "rock")
-      || (computerSelection === "scissors" && playerSelection === "paper")
-
-    ) return "computer";
-  
-
-  else if 
-    (
-      (computerSelection === "rock" && playerSelection === "paper")
-      || (computerSelection === "paper" && playerSelection == "scissors")
-      || (computerSelection === "scissors" && playerSelection === "rock")
-    ) return "player";
-
+  if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  )
+    return "computer";
+  else if (
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection == "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "rock")
+  )
+    return "player";
   else return "draw";
-}
+};
 
 const displayResult = (result) => {
-  const resultDisplay = document.querySelector('.result-display');
+  const resultDisplay = document.querySelector(".result-display");
   resultDisplay.textContent = result;
-}
+};
 
-// create a function called playRound that takes in playerSelection and computerSelection and returns a string that declares the winner
 const playRound = (playerSelection, computerSelection) => {
   const winner = getRoundWinner(playerSelection, computerSelection);
 
   if (winner === "draw") {
     displayResult("It's a draw");
-  }
-  
-  else if (winner === "computer") {
+  } else if (winner === "computer") {
     displayResult(`You lose! ${computerSelection} beats ${playerSelection}`);
-  }
-  
-  else {
+  } else {
     displayResult(`You win! ${playerSelection} beats ${computerSelection}`);
   }
-}
-
-const getUserHandShape = () => {
-  let userChosenHandShape;
-
-  const handShapes = ["rock", "paper", "scissors"];
-
-  // repeatedly run a while loop 
-  while (true) {
-    // ask the user "Indicate your hand shape?" and store the answer in userChosenHandShape
-    userChosenHandShape = prompt("Indicate your hand shape?");
-
-    // if userChosenHandShape is in handShapes
-    if (handShapes.includes(userChosenHandShape)) {
-      // break out of the loop
-      break;
-    }
-  }
-
-  return userChosenHandShape;
-}
+};
 
 let computerScore = 0;
 let playerScore = 0;
@@ -85,55 +50,58 @@ const game = (playerSelection, computerSelection) => {
 
   if (winner === "computer") computerScore++;
   else if (winner === "player") playerScore++;
-}
+};
 
 const getFinalResults = () => {
   if (computerScore > playerScore) {
-    return 'You lost!';
+    return "You lost!";
   } else if (computerScore < playerScore) {
-    return 'You won!';
-  } else return 'It\'s a draw!';
-}
+    return "You won!";
+  } else return "It's a draw!";
+};
 
 const updateUI = () => {
-  const playerScoreUI = document.querySelector('.player-score');
-  const computerScoreUI = document.querySelector('.computer-score');
-  const roundUI = document.querySelector('.round');
+  const playerScoreUI = document.querySelector(".player-score");
+  const computerScoreUI = document.querySelector(".computer-score");
+  const roundUI = document.querySelector(".round");
 
   playerScoreUI.textContent = playerScore;
   computerScoreUI.textContent = computerScore;
-  
+
   if (round > 5) {
-    const winnerDisplay = document.querySelector('.winner-display');
+    const winnerDisplay = document.querySelector(".winner-display");
     winnerDisplay.textContent = getFinalResults();
     toggleVisibilityOnResetButton();
     return;
   }
 
   roundUI.textContent = round;
-}
+};
 
 const resetGame = () => {
   computerScore = 0;
   playerScore = 0;
   round = 1;
 
-  const winnerDisplay = document.querySelector('.winner-display');
-  const resultDisplay = document.querySelector('.result-display');
+  const winnerDisplay = document.querySelector(".winner-display");
+  const resultDisplay = document.querySelector(".result-display");
 
-  winnerDisplay.textContent = '';
-  resultDisplay.textContent = '';
+  winnerDisplay.textContent = "";
+  resultDisplay.textContent = "";
 
   updateUI();
-}
+};
 
-// game();
+const toggleVisibilityOnResetButton = () => {
+  const resetButton = document.querySelector(".reset");
+  resetButton.classList.toggle("d-none");
+};
 
-const playButtons = document.querySelectorAll('.play-button');
+const playButtons = document.querySelectorAll(".play-button");
 
 playButtons.forEach((playButton) => {
-  playButton.addEventListener('click', () => {
-    if (round > 5) return; 
+  playButton.addEventListener("click", () => {
+    if (round > 5) return;
 
     const playerSelection = playButton.dataset.hand;
     const computerSelection = getComputerChoice();
@@ -141,15 +109,10 @@ playButtons.forEach((playButton) => {
     game(playerSelection, computerSelection);
     updateUI();
   });
-})
+});
 
-const toggleVisibilityOnResetButton = () => {
-  const resetButton = document.querySelector('.reset');
-  resetButton.classList.toggle('d-none');
-}
-
-const resetButton = document.querySelector('.reset');
-resetButton.addEventListener('click', () => {
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
   resetGame();
   toggleVisibilityOnResetButton();
 });
